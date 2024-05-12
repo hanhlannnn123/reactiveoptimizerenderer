259dde92e104b6184968b076055023087344d5d7
hanhlannnn123 <nazsanderspun0suy20@hotmail.com>
2024-05-12 21:32:23 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-function removeNthFromEnd(head, n) {
-  const dummy = new ListNode(0);
-  dummy.next = head;
-  let first = dummy;
-  let second = dummy;
-  for (let i = 1; i <= n + 1; i++) {
-    first = first.next;
-  }
-  while (first !== null) {
-    first = first.next;
-    second = second.next;
-  }
-  second.next = second.next.next;
-  return dummy.next;
+function greet(name) {
+  console.log("Hello, " + name + "!");
 }
+
+greet("Bob");
